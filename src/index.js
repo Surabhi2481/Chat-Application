89d55066779ac592a1cc6ad8ed31e98cfc411128
Surabhi2481 <surabhibbsr2481@gmail.com>
2021-01-23 22:30:23 +0530

    broadcasting events and sharing location

--- src/index.js
+++ src/index.js
@@ -12,27 +12,25 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-// server (emit) -> client (receive) - countUpdated
-// client (emit) -> server (receive) - increment
-
 io.on('connection', (socket) => {
-    console.log('New WebSocket Connection!')
+    console.log('New WebSocket connection')
 
-    socket.emit('message', 'Welcome')
+    socket.emit('message', 'Welcome!')
+    socket.broadcast.emit('message', 'A new user has joined!')
 
     socket.on('sendMessage', (message) => {
         io.emit('message', message)
     })
 
-    // socket.emit('countUpdated', count)
+    socket.on('sendLocation', (coords) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    })
 
-    // socket.on('increment', () => {
-    //     count++
-    //     //socket.emit('countUpdated', count)
-    //     io.emit('countUpdated',  count)
-    // })
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
+    })
 })
 
 server.listen(port, () => {
-    console.log('Server is up on port '+port+'!')
+    console.log(`Server is up on port ${port}!`)
 })
