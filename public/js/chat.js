36791d6533ba18fb0e4acc66aa4d4aa7ad0e86db
Surabhi2481 <surabhibbsr2481@gmail.com>
2021-01-24 14:12:13 +0530

    form and button states

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,26 @@
 const socket = io()
 
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
+
 socket.on('message', (message) => {
     console.log(message)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
+
+    $messageFormButton.setAttribute('disabled', 'disabled')
 
     const message = e.target.elements.message.value
 
     socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
         if (error) {
             return console.log(error)
         }
@@ -19,6 +30,8 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
 })
 
 document.querySelector('#send-location').addEventListener('click', () => {
+    $sendLocationButton.setAttribute('disabled', 'disabled')
+
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser.')
     }
@@ -28,6 +41,7 @@ document.querySelector('#send-location').addEventListener('click', () => {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, () => {
+            $sendLocationButton.removeAttribute('disabled')
             console.log('Location shared!')  
         })
     })
