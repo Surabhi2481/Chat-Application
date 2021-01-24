11525b3921ed14dd45d9653ffc25f05e5bcf97e1
Surabhi2481 <surabhibbsr2481@gmail.com>
2021-01-24 23:21:02 +0530

    timestamps for text and location messages

---
+++ src/utils/messages.js
@@ -0,0 +1,20 @@
+const { text } = require("express");
+
+const generateMessage = (text) => {
+    return {
+        text,
+        createdAt: new Date().getTime()
+    }
+}
+
+const generateLocationMessage = (url) => {
+    return {
+        url,
+        createdAt: new Date().getTime()
+    }
+}
+
+module.exports = {
+    generateMessage,
+    generateLocationMessage
+}
